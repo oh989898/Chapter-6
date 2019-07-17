47f2a3895873168f06e42788743435566db7fff6
oh989898 <43698498+oh989898@users.noreply.github.com>
2019-07-17 22:26:04 +0800

    Initial commit

---
+++ app/src/main/java/com/byted/camp/todolist/ui/NoteViewHolder.java
@@ -0,0 +1,105 @@
+package com.byted.camp.todolist.ui;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.byted.camp.todolist.MainActivity;
+import com.byted.camp.todolist.NoteOperator;
+import com.byted.camp.todolist.R;
+import com.byted.camp.todolist.beans.Note;
+import com.byted.camp.todolist.beans.State;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * Created on 2019/1/23.
+ *
+ * @author xuyingyi@bytedance.com (Yingyi Xu)
+ */
+public class NoteViewHolder extends RecyclerView.ViewHolder {
+
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT =
+            new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss", Locale.ENGLISH);
+
+    private final NoteOperator operator;
+    private CheckBox checkBox;
+    public TextView contentText;
+    private TextView dateText;
+    private View deleteBtn;
+
+    public NoteViewHolder(@NonNull View itemView, NoteOperator operator) {
+        super(itemView);
+        this.operator = operator;
+        checkBox = itemView.findViewById(R.id.checkbox);
+        contentText = itemView.findViewById(R.id.text_content);
+        dateText = itemView.findViewById(R.id.text_date);
+        deleteBtn = itemView.findViewById(R.id.btn_delete);
+    }
+
+    public void bind(final Note note) {
+        contentText.setText(note.getContent());
+        dateText.setText(SIMPLE_DATE_FORMAT.format(note.getDate()));
+
+        checkBox.setOnCheckedChangeListener(null);
+        checkBox.setChecked(note.getState() == State.DONE);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                note.setState(isChecked ? State.DONE : State.TODO);
+                operator.updateNote(note);
+            }
+        });
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operator.deleteNote(note);
+            }
+        });
+
+        contentText.setOnClickListener(new TextView.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog=new AlertDialog.Builder(v.getContext());
+                dialog.setTitle("Details");
+                dialog.setMessage(note.getTitle());
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+        if (note.getState() == State.DONE) {
+            contentText.setTextColor(Color.GRAY);
+            contentText.setPaintFlags(contentText.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        } else {
+            contentText.setTextColor(Color.BLACK);
+            contentText.setPaintFlags(contentText.getPaintFlags() & ~Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+
+        if(note.getPriority().color == Color.WHITE)
+        {
+            itemView.setBackgroundResource(R.drawable.whitebutton_selector);
+        }else if (note.getPriority().color == Color.RED)
+        {
+            itemView.setBackgroundResource(R.drawable.redbuton_selector);
+        }else
+        {
+            itemView.setBackgroundResource(R.drawable.pinkbutton_selector);
+        }
+    }
+}
